a5fac48ba227945ac4afdaf091425bd5b3da392c
Neil Danson <neil.danson@gmail.com>
2025-01-22 09:49:30 +0000

    Add direction field to Ray struct and update constructor

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
 
 type Vec3 = glam::Vec3A;
 
+#[derive(Debug)]
 struct Ray {
     position : Vec3,
+    direction : Vec3,
 }
 
 impl Ray {
-    fn new(position: Vec3) -> Ray {
-        Ray { position }
+    fn new(position: Vec3, direction : Vec3) -> Ray {
+        Ray { position, direction }
     }
 }
 
 
 fn main() {
-    let ray = Ray::new(Vec3::new(0.0, 0.0, 0.0));
-    println!("Ray position: {:?}", ray.position);
+    let ray = Ray::new(Vec3::ZERO, Vec3::Y);
+    println!("Ray position: {:?}", ray);
 }
