2b705bc7e92cc7f9ef125054014697010d14aef8
Neil Danson <neil.danson@gmail.com>
2025-01-27 09:12:51 +0000

    Increase resolution to 1920x1080 and refactor normal calculation for improved precision

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
+use std::f32;
+
 use image::RgbImage;
 use rayon::prelude::*;
 
 type Vec3 = glam::Vec3A;
 
-const WIDTH: u32 = 1280;
-const HEIGHT: u32 = 720;
+const WIDTH: u32 = 1920;
+const HEIGHT: u32 = 1080;
 const IMAGE_SIZE: u32 = WIDTH * HEIGHT;
 const MAX_DEPTH: f32 = 50.0f32;
 const INV_WIDTH: f32 = 1.0 / WIDTH as f32;
 const INV_HEIGHT: f32 = 1.0 / HEIGHT as f32;
+
+const MIN_DISTANCE : f32 = 0.001;
+const VEC3_EPSILON_X : Vec3 = Vec3::new(MIN_DISTANCE, 0.0, 0.0);
+const VEC3_EPSILON_Y : Vec3 = Vec3::new(0.0, MIN_DISTANCE, 0.0);
+const VEC3_EPSILON_Z : Vec3 = Vec3::new(0.0, 0.0, MIN_DISTANCE);
 
 #[derive(Debug)]
 struct Ray {
@@ -27,15 +34,11 @@ impl Ray {
 
 trait Sdf: Sync + Send {
     fn distance(&self, point: Vec3) -> f32;
-    fn normal(&self, point: Vec3) -> Vec3 {
-        let eps = 0.0001;
-        let x = Vec3::new(eps, 0.0, 0.0);
-        let y = Vec3::new(0.0, eps, 0.0);
-        let z = Vec3::new(0.0, 0.0, eps);
+    fn normal(&self, point: Vec3) -> Vec3 {        
         let normal = Vec3::new(
-            self.distance(point + x) - self.distance(point - x),
-            self.distance(point + y) - self.distance(point - y),
-            self.distance(point + z) - self.distance(point - z),
+            self.distance(point + VEC3_EPSILON_X) - self.distance(point - VEC3_EPSILON_X),
+            self.distance(point + VEC3_EPSILON_Y) - self.distance(point - VEC3_EPSILON_Y),
+            self.distance(point + VEC3_EPSILON_Z) - self.distance(point - VEC3_EPSILON_Z),
         );
         normal.normalize()
     }
@@ -87,7 +90,7 @@ fn trace_ray(ray: Ray, shapes: &Vec<&dyn Sdf>) -> Vec3 {
             break;
         }
         p = p + ray.direction * min_distance;
-        if min_distance < 0.001 {
+        if min_distance < MIN_DISTANCE {
             let normal = shapes[0].normal(p);
             let color_vec = (normal + Vec3::ONE) * 0.5;
             return color_vec;
@@ -112,13 +115,14 @@ fn main() {
 
     let result: Vec<_> = (0..IMAGE_SIZE)
         .into_par_iter()
+        //.chunks(8)
         .map(|pos| {
             let x = pos % WIDTH;
             let y = pos / WIDTH;
             let x = (x as f32) * INV_WIDTH * 2.0 - 1.0;
             let y = (y as f32) * INV_HEIGHT * 2.0 - 1.0;
             let x = x * aspect_ratio;
-            let ray = Ray::new(Vec3::new(0.0, 0.0, 0.0), Vec3::new(x, y, 1.0).normalize());
+            let ray = Ray::new(Vec3::ZERO, Vec3::new(x, y, 1.0).normalize());
             trace_ray(ray, &shapes)
         })
         .collect();
