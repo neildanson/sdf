f14b33faf52c1a62fbead8aa92d051cda682296b
Neil Danson <neil.danson@gmail.com>
2025-01-27 09:30:40 +0000

    Refactor floating-point type usage for consistency and precision

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
-use std::f32;
-
 use image::RgbImage;
 use rayon::prelude::*;
 
 type Vec3 = glam::Vec3A;
+type FLOAT = f32;
 
 const WIDTH: u32 = 1920;
 const HEIGHT: u32 = 1080;
 const IMAGE_SIZE: u32 = WIDTH * HEIGHT;
-const MAX_DEPTH: f32 = 50.0f32;
-const INV_WIDTH: f32 = 1.0 / WIDTH as f32;
-const INV_HEIGHT: f32 = 1.0 / HEIGHT as f32;
+const MAX_DEPTH: FLOAT = 50.0;
+const INV_WIDTH: FLOAT = 1.0 / WIDTH as FLOAT;
+const INV_HEIGHT: FLOAT = 1.0 / HEIGHT as FLOAT;
 
-const MIN_DISTANCE: f32 = 0.001;
+const MIN_DISTANCE: FLOAT = 0.001;
 const VEC3_EPSILON_X: Vec3 = Vec3::new(MIN_DISTANCE, 0.0, 0.0);
 const VEC3_EPSILON_Y: Vec3 = Vec3::new(0.0, MIN_DISTANCE, 0.0);
 const VEC3_EPSILON_Z: Vec3 = Vec3::new(0.0, 0.0, MIN_DISTANCE);
@@ -33,7 +32,7 @@ impl Ray {
 }
 
 trait Sdf: Sync + Send {
-    fn distance(&self, point: Vec3) -> f32;
+    fn distance(&self, point: Vec3) -> FLOAT;
     fn normal(&self, point: Vec3) -> Vec3 {
         let normal = Vec3::new(
             self.distance(point + VEC3_EPSILON_X) - self.distance(point - VEC3_EPSILON_X),
@@ -46,22 +45,22 @@ trait Sdf: Sync + Send {
 
 struct Sphere {
     center: Vec3,
-    radius: f32,
+    radius: FLOAT,
 }
 
 impl Sdf for Sphere {
-    fn distance(&self, point: Vec3) -> f32 {
+    fn distance(&self, point: Vec3) -> FLOAT {
         (point - self.center).length() - self.radius
     }
 }
 
 struct Cube {
     center: Vec3,
-    size: f32,
+    size: FLOAT,
 }
 
 impl Sdf for Cube {
-    fn distance(&self, point: Vec3) -> f32 {
+    fn distance(&self, point: Vec3) -> FLOAT {
         let q = (point - self.center).abs() - Vec3::splat(self.size);
         q.max(Vec3::ZERO).length() + q.max_element().min(0.0)
     }
@@ -78,7 +77,7 @@ fn to_color(col: Vec3) -> [u8; 3] {
 fn trace_ray(ray: Ray, shapes: &Vec<&dyn Sdf>) -> Vec3 {
     let mut p = ray.position;
     loop {
-        let mut min_distance = f32::MAX;
+        let mut min_distance = FLOAT::MAX;
         for shape in shapes {
             let d = shape.distance(p);
             if d < min_distance {
@@ -109,21 +108,22 @@ fn main() {
     };
 
     let shapes: Vec<&dyn Sdf> = vec![&sphere, &cube];
-    let aspect_ratio = WIDTH as f32 / HEIGHT as f32;
+    let aspect_ratio = WIDTH as FLOAT / HEIGHT as FLOAT;
+    let mut result = vec![Vec3::ZERO; IMAGE_SIZE as usize];
     let start = std::time::Instant::now();
 
-    let result: Vec<Vec3> = (0..IMAGE_SIZE)
+    (0..IMAGE_SIZE)
         .into_par_iter()
         .map(|pos| {
             let x = pos % WIDTH;
             let y = pos / WIDTH;
-            let x = (x as f32) * INV_WIDTH * 2.0 - 1.0;
-            let y = (y as f32) * INV_HEIGHT * 2.0 - 1.0;
+            let x = (x as FLOAT) * INV_WIDTH * 2.0 - 1.0;
+            let y = (y as FLOAT) * INV_HEIGHT * 2.0 - 1.0;
             let x = x * aspect_ratio;
             let ray = Ray::new(Vec3::ZERO, Vec3::new(x, y, 1.0).normalize());
             trace_ray(ray, &shapes)
         })
-        .collect();
+        .collect_into_vec(&mut result);
 
     let elapsed = start.elapsed();
 
